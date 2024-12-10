bb5b21acb60a05fb303faf2a0b5fc4a61b4447c3
Maksym Adamenko <maimav56@gmail.com>
2024-12-11 01:04:24 +0200

    hw done

--- src/main.jsx
+++ src/main.jsx
@@ -8,11 +8,7 @@ import App from "./components/App";
 import { store, persistor } from "./redux/store";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import "modern-normalize";
-import "reset-css";
 import "./index.css";
-
-const GOOGLE_CLIENT_ID =
-  "138184153676-m1bf4m8uusd41sguqps8t1plvdf4oj5s.apps.googleusercontent.com";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
